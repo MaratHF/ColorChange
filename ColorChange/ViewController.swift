932b2216207042d077318b6cc93ec177da77f531
Marat Khayrullin <m.f.khayrullin@gmail.com>
2022-03-05 01:07:48 +0300

    writing code

--- ColorChange/ViewController.swift
+++ ColorChange/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redColorValue: UILabel!
+    @IBOutlet var greenColorValue: UILabel!
+    @IBOutlet var blueColorValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+
+    @IBAction func redSliderAction() {
+        redColorValue.text = String(round(100 * redSlider.value) / 100)
     }
-
-
+    
+    @IBAction func greenSliderAction() {
+        greenColorValue.text = String(round(100 * greenSlider.value) / 100)
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueColorValue.text = String(round(100 * blueSlider.value) / 100)
+    }
+    
 }
 
