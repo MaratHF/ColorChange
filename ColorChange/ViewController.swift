63c8b29eded08b20590ce50dab27eeb87088d366
Marat Khayrullin <m.f.khayrullin@gmail.com>
2022-03-05 19:24:36 +0300

    App is done

--- ColorChange/ViewController.swift
+++ ColorChange/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var colorView: UIView!
     
     @IBOutlet var redColorValue: UILabel!
@@ -19,20 +19,28 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//    }
-
     @IBAction func redSliderAction() {
         redColorValue.text = String(round(100 * redSlider.value) / 100)
+        changeColorView()
     }
     
     @IBAction func greenSliderAction() {
         greenColorValue.text = String(round(100 * greenSlider.value) / 100)
+        changeColorView()
     }
     
     @IBAction func blueSliderAction() {
         blueColorValue.text = String(round(100 * blueSlider.value) / 100)
+        changeColorView()
+    }
+    
+    private func changeColorView() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
     }
     
 }
